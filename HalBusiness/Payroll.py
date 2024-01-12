eaa483c44a1d9e1c015c631ca582a34553c7aede
Enio Hysa <eniohysa05@gmail.com>
2024-01-12 11:52:35 -0500

    comments added

--- HalBusiness/Payroll.py
+++ HalBusiness/Payroll.py
@@ -1,4 +1,5 @@
 class Payroll:
+    # Initialize with all the variables
     def __init__(self, employee_id, employee_first_name, employee_last_name, sales_amt, advanced_pay):
         self.employee_id = employee_id
         self.employee_first_name = employee_first_name
@@ -7,6 +8,7 @@ class Payroll:
         self.advanced_pay = advanced_pay
 
     def calc_pay(self):
+        # Find commission based on sales amount
         if self.sales_amt >= 22000:
             commission = 0.18
         elif self.sales_amt >= 18000:
@@ -18,10 +20,12 @@ class Payroll:
         else:
             commission = 0.10
 
+        # Calculate pay with previously found commission rate
         pay = self.sales_amt * commission - self.advanced_pay
         return pay
 
     def display(self):
+        # Print all the info
         print(f'Name: {self.employee_first_name} {self.employee_last_name}')
         print(f'ID: {self.employee_id}')
         print(f'Sales Amount: {self.sales_amt}')
