bdbf899cbe8ef4b22163c64c56ba3802590fc0e3
Enio Hysa <eniohysa05@gmail.com>
2024-01-12 11:49:27 -0500

    testing run data hw1

--- HW1/Grade.py
+++ HW1/Grade.py
@@ -1,4 +1,5 @@
 def calc_average(scores):
+    # Calculate the average by iterating through the inputted list and adding the scores to a counter
     counter = 0
     for score in scores:
         counter += int(score)
@@ -6,8 +7,10 @@ def calc_average(scores):
 
 
 def converter(grade):
+    # Make sure score from calc_average function is valid
     if grade < 0 or grade > 100:
         letter_grade = 'Invalid. Grade must be from 0-100.'
+    # Assign letter grade based on score
     elif grade >= 90:
         letter_grade = 'A'
     elif grade >= 80:
--- HW1/Grade_Tester.py
+++ HW1/Grade_Tester.py
@@ -8,13 +8,17 @@ import Grade
 
 
 def main():
+    # Create a list to store user scores
     scores = []
     for i in range(5):
+        # Get 5 scores from user and append them to a list
         score = int(input(f'Enter grade #{i + 1}: '))
+        # Make sure the scores are valid
         while score > 100 or score < 0:
             score = int(input(f'Must be between 0-100. Enter grade {i + 1}: '))
         scores.append(score)
 
+    # Call the functions from the Grade module and print the average and letter grade
     average = Grade.calc_average(scores)
     letter_grade = Grade.converter(average)
     print(f'Average: {average}\nLetter Grade: {letter_grade}')
@@ -23,8 +27,22 @@ def main():
 if __name__ == '__main__':
     main()
     while True:
+        # Prompt user to rerun the code by typing Y or y
         rerun = input(f'Type \'Y\' to try new grades. Type anything else to close\n')
         if rerun == 'Y' or rerun == 'y':
             main()
         else:
             break
+
+''' Testing Run Data:
+Enter grade #1: 101
+Must be between 0-100. Enter grade 1: 100
+Enter grade #2: 97
+Enter grade #3: 99
+Enter grade #4: 45
+Enter grade #5: 89
+Average: 86.0
+Letter Grade: B
+Type 'Y' to try new grades. Type anything else to close
+n
+'''
