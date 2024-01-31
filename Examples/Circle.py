2a20992ec029b7219850e0990cc23a9f9a4a647a
Enio Hysa <eniohysa05@gmail.com>
2024-01-30 20:35:40 -0500

    idek

--- Examples/Circle.py
+++ Examples/Circle.py
@@ -7,15 +7,18 @@ import math
 
 
 class Circle:
-    def __init__(self):
+    def __init__(self, r):
         # Constructor
-        self.__radius = 0.0  # Private data
+        self.__radius = r  # Private data
 
-    def set_radius(self, r):
-        self.__radius = r
+    def __str__(self):
+        return f'Circle rad={self.__radius:.2f}\narea={self.calc_area():.2f}\nperimeter={self.calc_perimeter():.2f}\n'
 
     def get_radius(self):
         return self.__radius
+
+    def set_radius(self, r):
+        self.__radius = r
 
     def calc_area(self):
         area = self.__radius ** 2 * math.pi
@@ -28,11 +31,17 @@ class Circle:
     def display(self):
         print(f'The circle with radius = {self.__radius:.2f}\n'
               f'area = {self.calc_area():.2f}\n'
-              f'perimeter = {self.calc_perimeter():.2f}')
+              f'perimeter = {self.calc_perimeter():.2f}\n')
 
 
 if __name__ == '__main__':
     # Circle 1, radius = 5
-    circle1 = Circle()
-    circle1.set_radius(5)
+    circle1 = Circle(r=5)
     circle1.display()
+
+    circle2 = Circle(r=10)
+    circle2.display()
+
+    circle3 = Circle(7)
+    circle3.set_radius(13)
+    circle3.display()
