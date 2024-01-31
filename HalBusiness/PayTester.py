429f395d3e750f550f705f772569595204525c4a
Enio Hysa <eniohysa05@gmail.com>
2024-01-30 21:12:27 -0500

    finished hw2

--- HalBusiness/PayTester.py
+++ HalBusiness/PayTester.py
@@ -10,7 +10,44 @@ import Payroll
 def main():
     employee1 = Payroll.Payroll(1234, "Enio", "Hysa", 25000, 0)
     employee1.display()
+    employee1.set_sales_amt(10000)
+    employee1.set_advanced_pay(1000)
+    print(f"Updated sales: ${employee1.get_sales_amt()}\n"
+          f"Updated advanced pay: ${employee1.get_advanced_pay()}\n"
+          f"Updated pay: ${employee1.calc_pay()}\n")
+
+    employee2 = Payroll.Payroll(1101, "John", "Doe", 14500, 200)
+    employee2.display()
+    employee2.set_employee_id(3690)
+    employee2.set_employee_first_name("Jane")
+    employee2.set_employee_last_name("Smith")
+    print(f"Updated employee ID: {employee2.get_employee_id()}\n"
+          f"Updated employee first name: {employee2.get_employee_first_name()}\n"
+          f"Updated employee last_name: {employee2.get_employee_last_name()}\n")
 
 
 if __name__ == '__main__':
     main()
+
+'''Testing Data:
+Name: Enio Hysa
+ID: 1234
+Sales Amount: $25000
+Advanced Pay: $0    
+Total Pay: $4500.0
+
+Updated sales: $10000
+Updated advanced pay: $1000
+Updated pay: $200.0
+
+Name: John Doe
+ID: 1101
+Sales Amount: $14500
+Advanced Pay: $200
+Total Pay: $1540.0
+
+Updated employee ID: 3690
+Updated employee first name: Jane
+Updated employee last_name: Smith
+
+'''
--- HalBusiness/Payroll.py
+++ HalBusiness/Payroll.py
@@ -1,33 +1,67 @@
 class Payroll:
     # Initialize with all the variables
     def __init__(self, employee_id, employee_first_name, employee_last_name, sales_amt, advanced_pay):
-        self.employee_id = employee_id
-        self.employee_first_name = employee_first_name
-        self.employee_last_name = employee_last_name
-        self.sales_amt = sales_amt
-        self.advanced_pay = advanced_pay
+        self.__employee_id = employee_id
+        self.__employee_first_name = employee_first_name
+        self.__employee_last_name = employee_last_name
+        self.__sales_amt = sales_amt
+        self.__advanced_pay = advanced_pay
 
+    # Setter functions
+    def set_employee_id(self, employee_id):
+        self.__employee_id = employee_id
+
+    def set_employee_first_name(self, employee_first_name):
+        self.__employee_first_name = employee_first_name
+
+    def set_employee_last_name(self, employee_last_name):
+        self.__employee_last_name = employee_last_name
+
+    def set_sales_amt(self, sales_amt):
+        self.__sales_amt = sales_amt
+
+    def set_advanced_pay(self, advanced_pay):
+        self.__advanced_pay = advanced_pay
+
+    # Getter functions
+    def get_employee_id(self):
+        return self.__employee_id
+
+    def get_employee_first_name(self):
+        return self.__employee_first_name
+
+    def get_employee_last_name(self):
+        return self.__employee_last_name
+
+    def get_sales_amt(self):
+        return self.__sales_amt
+
+    def get_advanced_pay(self):
+        return self.__advanced_pay
+
+    # Calculate pay
     def calc_pay(self):
         # Find commission based on sales amount
-        if self.sales_amt >= 22000:
+        if self.__sales_amt >= 22000:
             commission = 0.18
-        elif self.sales_amt >= 18000:
+        elif self.__sales_amt >= 18000:
             commission = 0.16
-        elif self.sales_amt >= 15000:
+        elif self.__sales_amt >= 15000:
             commission = 0.14
-        elif self.sales_amt >= 10000:
+        elif self.__sales_amt >= 10000:
             commission = 0.12
         else:
             commission = 0.10
 
         # Calculate pay with previously found commission rate
-        pay = self.sales_amt * commission - self.advanced_pay
+        pay = self.__sales_amt * commission - self.__advanced_pay
         return pay
 
+    # Print all the info
     def display(self):
         # Print all the info
-        print(f'Name: {self.employee_first_name} {self.employee_last_name}')
-        print(f'ID: {self.employee_id}')
-        print(f'Sales Amount: {self.sales_amt}')
-        print(f'Advanced Pay: {self.advanced_pay}')
-        print(f'Total Pay: {self.calc_pay()}')
+        print(f'Name: {self.__employee_first_name} {self.__employee_last_name}')
+        print(f'ID: {self.__employee_id}')
+        print(f'Sales Amount: ${self.__sales_amt}')
+        print(f'Advanced Pay: ${self.__advanced_pay}')
+        print(f'Total Pay: ${self.calc_pay()}\n')
